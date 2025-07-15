a8562d66259e46540f9d1429346ba426f1372218
audrius777 <travcen.com@gmail.com>
2025-07-15 10:45:31 +0300

    Add files via upload

--- backendas/auth.js
+++ backendas/auth.js
@@ -1,23 +1,24 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-require('dotenv').config();
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // Galima išsaugoti user duomenis į DB čia
-    console.log("Google profile:", profile);
-    return done(null, profile);
-  }
-));
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get("/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => res.redirect("/")
+);
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+router.get("/facebook", passport.authenticate("facebook", { scope: ["email"] }));
+router.get("/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/" }),
+  (req, res) => res.redirect("/")
+);
+
+router.get("/logout", (req, res) => {
+  req.logout(err => {
+    if (err) return res.status(500).send("Atsijungimo klaida");
+    req.session.destroy(() => res.redirect("/"));
+  });
 });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+module.exports = router;
