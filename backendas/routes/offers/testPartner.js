c01924836290e8c3612d100b3e3413534e201662
audrius777 <travcen.com@gmail.com>
2025-07-15 10:48:32 +0300

    Add files via upload

---
+++ backendas/routes/offers/testPartner.js
@@ -0,0 +1,11 @@
+module.exports = async function fetchTestPartnerOffers() {
+  return [
+    {
+      title: "Trip to Tokyo",
+      price: 799,
+      url: "https://testpartner.com/trip/tokyo",
+      image: "https://source.unsplash.com/280x180/?tokyo",
+      partner: "TestPartner"
+    }
+  ];
+};
