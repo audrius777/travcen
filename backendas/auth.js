d7d3788a2ff1bceaa2a44f3a686becf6e203cecf
audrius777 <travcen.com@gmail.com>
2025-07-15 08:53:48 +0300

    Add files via upload

---
+++ backendas/auth.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // Galima išsaugoti user duomenis į DB čia
+    console.log("Google profile:", profile);
+    return done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
