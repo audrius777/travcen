3054fabf4184dcf36a8ca3a320fbafdf1977650d
audrius777 <travcen.com@gmail.com>
2025-07-16 11:47:45 +0300

    Delete backendas directory

--- backendas/services/aggregateOffers.js
+++
@@ -1,9 +0,0 @@
-const getExamplePartnerOffers = require("../partners/example_partner");
-
-module.exports = async function aggregateOffers() {
-  const allOffers = await Promise.all([
-    getExamplePartnerOffers()
-  ]);
-
-  return allOffers.flat();
-};
