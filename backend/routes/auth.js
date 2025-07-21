005d4edba1c124b5606e4fdc568267c67b84897f
audrius777 <travcen.com@gmail.com>
2025-07-21 16:18:10 +0300

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,23 +1,75 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
+const { logAuthEvent } = require("../utils/logger");
+const { validateSession } = require("../middleware/auth");
 
-router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-router.get("/google/callback",
-  passport.authenticate("google", { failureRedirect: "/" }),
-  (req, res) => res.redirect("/")
+// Konfigūracija
+const AUTH_CONFIG = {
+  google: {
+    scope: ["profile", "email"],
+    failureRedirect: "/prisijungimas?klaida=autentifikacija",
+    successRedirect: "/dashboard"
+  },
+  facebook: {
+    scope: ["email"],
+    failureRedirect: "/prisijungimas?klaida=autentifikacija",
+    successRedirect: "/dashboard"
+  }
+};
+
+// Google autentifikacijos maršrutai
+router.get("/google", (req, res, next) => {
+  logAuthEvent("google_autentifikacija_pradeta", { ip: req.ip });
+  passport.authenticate("google", AUTH_CONFIG.google)(req, res, next);
+});
+
+router.get("/google/callback", 
+  passport.authenticate("google", AUTH_CONFIG.google),
+  (req, res) => {
+    logAuthEvent("google_autentifikacija_pavyko", { userId: req.user.id });
+    res.redirect(AUTH_CONFIG.google.successRedirect);
+  }
 );
 
-router.get("/facebook", passport.authenticate("facebook", { scope: ["email"] }));
+// Facebook autentifikacijos maršrutai
+router.get("/facebook", (req, res, next) => {
+  logAuthEvent("facebook_autentifikacija_pradeta", { ip: req.ip });
+  passport.authenticate("facebook", AUTH_CONFIG.facebook)(req, res, next);
+});
+
 router.get("/facebook/callback",
-  passport.authenticate("facebook", { failureRedirect: "/" }),
-  (req, res) => res.redirect("/")
+  passport.authenticate("facebook", AUTH_CONFIG.facebook),
+  (req, res) => {
+    logAuthEvent("facebook_autentifikacija_pavyko", { userId: req.user.id });
+    res.redirect(AUTH_CONFIG.facebook.successRedirect);
+  }
 );
 
-router.get("/logout", (req, res) => {
-  req.logout(err => {
-    if (err) return res.status(500).send("Atsijungimo klaida");
-    req.session.destroy(() => res.redirect("/"));
+// Atsijungimo maršrutas
+router.get("/atsijungti", validateSession, (req, res) => {
+  const userId = req.user?.id;
+  
+  req.logout((err) => {
+    if (err) {
+      logAuthEvent("atsijungti_nepavyko", { userId, error: err.message });
+      return res.status(500).render("klaida", { 
+        message: "Atsijungimo klaida" 
+      });
+    }
+
+    req.session.destroy((err) => {
+      if (err) {
+        logAuthEvent("sesijos_sunaikinimas_nepavyko", { userId, error: err.message });
+        return res.status(500).render("klaida", {
+          message: "Sesijos sunaikinimo klaida"
+        });
+      }
+      
+      logAuthEvent("atsijungimas_pavyko", { userId });
+      res.clearCookie("connect.sid");
+      res.redirect("/prisijungimas?atsijungta=sėkmingai");
+    });
   });
 });
 
