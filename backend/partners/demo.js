894e7655216e29a3dcc0b46cc90db71e1794c75e
audrius777 <travcen.com@gmail.com>
2025-10-08 14:48:28 +0300

    Add files via upload

--- backend/partners/demo.js
+++ backend/partners/demo.js
@@ -1,1 +1,14 @@
-
+module.exports = async function demoOffers() {
+  return [
+    {
+      title: "Demo Kelionė į Romą",
+      from: "Vilnius",
+      to: "Roma",
+      type: "cultural",
+      price: 299,
+      url: "https://www.colibri-reiseagentur.com/",
+      image: "https://source.unsplash.com/280x180/?rome",
+      partner: "DemoPartner" // Added for consistency
+    }
+  ];
+};
