49da0be26619730f4cfa60968d1bd8496e6c1416
audrius777 <travcen.com@gmail.com>
2025-10-09 12:46:04 +0300

    Delete backend/offerProcessor.js

--- backend/offerProcessor.js
+++
@@ -1,6 +0,0 @@
-// offerProcessor.js (supaprastinta versija, jei vis dar reikalinga)
-import { PartnerOffer } from '../models/offerModel.js';
-
-export async function processSingleOffer(offer) {
-  return new PartnerOffer(offer); // DB schema automatiškai apdoros konvertavimą
-}
